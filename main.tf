07b315369f35d9cc6aeee7f0d76983d5ee20a3dc
EC2 Default User <ec2-user@ip-172-31-52-51.us-west-2.compute.internal>
2024-02-06 23:30:33 +0000

    updating main.tf

--- main.tf
+++ main.tf
@@ -12,19 +12,19 @@ provider "aws" {
     region="us-west-2"
 }
 
-resource "aws_ecr_repository" "group5_capstone1_repo" {
-  name = "group5-capstone1-repo" # Naming my repository
+resource "aws_ecr_repository" "g5_capstone2_ecr_tf" {
+  name = "g5-capstone2-ecr-tf" # Naming my repository
 }
-resource "aws_ecs_cluster" "group5_capstone1_cluster" {
-  name = "group5-capstone1-cluster" # Naming the cluster
+resource "aws_ecs_cluster" "g5_ecs_capstone2_tf" {
+  name = "g5-ecs-capstone2-tf" # Naming the cluster
 }
-resource "aws_ecs_task_definition" "group5_capstone1_task" {
-  family                   = "group5-capstone1-task" # Naming our first task
+resource "aws_ecs_task_definition" "g5_taskdef_capstone2_tf" {
+  family                   = "g5-taskdef-capstone2-tf" # Naming our first task
   container_definitions    = <<DEFINITION
   [
     {
-      "name": "group5-capstone1-task",
-      "image": "${aws_ecr_repository.group5_capstone1_repo.repository_url}",
+      "name": "g5-taskdef-capstone2-tf",
+      "image": "${aws_ecr_repository.g5_capstone2_ecr_tf.repository_url}",
       "essential": true,
       "portMappings": [
         {
@@ -41,42 +41,15 @@ resource "aws_ecs_task_definition" "group5_capstone1_task" {
   network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
   memory                   = 512         # Specifying the memory our container requires
   cpu                      = 256         # Specifying the CPU our container requires
-  execution_role_arn       = "${aws_iam_role.group5ecsTaskExecutionRole.arn}"
+  execution_role_arn       = "arn:aws:iam::962804699607:role/ecsTaskExecutionRole"
 }
 
-resource "aws_iam_role" "group5ecsTaskExecutionRole" {
-  name               = "group5ecsTaskExecutionRole"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
-}
-
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "group5ecsTaskExecutionRole_policy" {
-  role       = "${aws_iam_role.group5ecsTaskExecutionRole.name}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_ecs_service" "group5_service" {
-  name            = "group5-service"                             # Naming our first service
-  cluster         = "${aws_ecs_cluster.group5_capstone1_cluster.id}"             # Referencing our created Cluster
-  task_definition = "${aws_ecs_task_definition.group5_capstone1_task.arn}" # Referencing the task our service will spin up
+resource "aws_ecs_service" "g5_service_capstone2_tf" {
+  name            = "g5-service-capstone2-tf"                             # Naming our first service
+  cluster         = "${aws_ecs_cluster.g5_ecs_capstone2_tf.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.g5_taskdef_capstone2_tf.arn}" # Referencing the task our service will spin up
   launch_type     = "FARGATE"
-  desired_count   = 3 # Setting the number of containers we want deployed to 3
-  
-  load_balancer {
-    target_group_arn = "${aws_lb_target_group.group5target_group.arn}" # Referencing our target group
-    container_name   = "${aws_ecs_task_definition.group5_capstone1_task.family}"
-    container_port   = 3000 # Specifying the container port
-  }
+  desired_count   = 1 # Setting the number of containers we want deployed to 3
   
   network_configuration {
     subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
@@ -87,6 +60,7 @@ resource "aws_ecs_service" "group5_service" {
 # Providing a reference to our default VPC
 resource "aws_default_vpc" "default_vpc" {
 }
+
 
 # Providing a reference to our default subnets
 resource "aws_default_subnet" "default_subnet_a" {
@@ -99,55 +73,4 @@ resource "aws_default_subnet" "default_subnet_b" {
 
 resource "aws_default_subnet" "default_subnet_c" {
   availability_zone = "us-west-2c"
-}
-
-resource "aws_alb" "group5application_load_balancer" {
-  name               = "group5-lb-tf" # Naming our load balancer
-  load_balancer_type = "application"
-  subnets = [ # Referencing the default subnets
-    "${aws_default_subnet.default_subnet_a.id}",
-    "${aws_default_subnet.default_subnet_b.id}",
-    "${aws_default_subnet.default_subnet_c.id}"
-  ]
-  # Referencing the security group
-  security_groups = ["${aws_security_group.group5load_balancer_security_group.id}"]
-}
-
-# Creating a security group for the load balancer:
-resource "aws_security_group" "group5load_balancer_security_group" {
-  ingress {
-    from_port   = 80 # Allowing traffic in from port 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
-  }
-
-  egress {
-    from_port   = 0 # Allowing any incoming port
-    to_port     = 0 # Allowing any outgoing port
-    protocol    = "-1" # Allowing any outgoing protocol 
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
-  }
-}
-
-resource "aws_lb_target_group" "group5target_group" {
-  name        = "group5target-group"
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
-  health_check {
-    matcher = "200,301,302"
-    path = "/"
-  }
-}
-
-resource "aws_lb_listener" "group5listener" {
-  load_balancer_arn = "${aws_alb.group5application_load_balancer.arn}" # Referencing our load balancer
-  port              = "80"
-  protocol          = "HTTP"
-  default_action {
-    type             = "forward"
-    target_group_arn = "${aws_lb_target_group.group5target_group.arn}" # Referencing our tagrte group
-  }
 }
