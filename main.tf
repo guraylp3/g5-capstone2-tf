b2e5a48a41d71df35d5a5ba1f2ba270f19d9e2ab
EC2 Default User <ec2-user@ip-172-31-52-51.us-west-2.compute.internal>
2024-02-07 22:12:23 +0000

    policies and roles for codebuild and code pipeline

--- main.tf
+++ main.tf
@@ -80,11 +80,82 @@ resource "aws_default_subnet" "default_subnet_c" {
   availability_zone = "us-west-2c"
 }
 
+// CodeBuild Role
+resource "aws_iam_role_policy" "g5_codebuild_policy_tf" {
+  name   = "g5-codebuild-policy-tf"
+  role   = aws_iam_role.g5_codebuild_role_tf.id
+  policy = data.aws_iam_policy_document.codebuild_policy.json
+}
+ 
+resource "aws_iam_role" "g5_codebuild_role_tf" {
+  name               = "g5-codebuild-role-tf"
+  assume_role_policy = data.aws_iam_policy_document.codebuild_assume_role.json
+}
+ 
+data "aws_iam_policy_document" "codebuild_policy" {
+  statement {
+    effect = "Allow"
+ 
+    actions = [
+      "logs:CreateLogStream",
+      "logs:CreateLogGroup",
+      "logs:PutLogEvents",
+      "s3:*"
+    ]
+ 
+    resources = ["*"]
+  }
+ 
+  statement {
+    effect    = "Allow"
+    actions   = ["codestar-connections:UseConnection"]
+    resources = ["arn:aws:codestar-connections:us-west-2:962804699607:connection/5bbfbc52-4a3e-4124-a0ee-daf366f4ec2b"]
+  }
+ 
+  statement {
+    effect = "Allow"
+ 
+    actions = [
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild",
+      "codebuild:CreateReportGroup",
+      "codebuild:CreateReport",
+      "codebuild:UpdateReport",
+      "codebuild:BatchPutTestCases",
+      "codebuild:BatchPutCodeCoverages",
+      "ecr:CompleteLayerUpload",
+      "ecr:GetAuthorizationToken",
+      "ecr:UploadLayerPart",
+      "ecr:InitiateLayerUpload",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage"
+    ]
+ 
+    resources = ["*"]
+  }
+}
+ 
+data "aws_iam_policy_document" "codebuild_assume_role" {
+  statement {
+    effect = "Allow"
+ 
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+ 
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+
+
+
 resource "aws_codebuild_project" "g5_capstone2_codebuild_tf" {
   name          = "g5-capstone2-codebuild-tf"
   description   = "g5-capstone2-codebuild-tf"
   build_timeout = 5
-  service_role  = "arn:aws:iam::962804699607:role/service-role/codebuild-g5-capstone2-codebuild-service-role"
+  service_role  = aws_iam_role.g5_codebuild_role_tf.arn
 
   artifacts {
     type = "NO_ARTIFACTS"
@@ -115,3 +186,139 @@ resource "aws_codebuild_project" "g5_capstone2_codebuild_tf" {
   source_version = "main"
 }
 
+# CodePipeline Role 
+resource "aws_iam_role_policy" "g5_codepipeline_policy_tf" {
+  name   = "g5-codepipeline-policy-tf"
+  role   = aws_iam_role.g5_codepipeline_role_tf.id
+  policy = data.aws_iam_policy_document.codepipeline_policy.json
+}
+ 
+resource "aws_iam_role" "g5_codepipeline_role_tf" {
+  name               = "g5-codepipeline-role-tf"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+ 
+data "aws_iam_policy_document" "codepipeline_policy" {
+  statement {
+    effect = "Allow"
+ 
+    actions = [
+      "s3:*",
+      "logs:CreateLogStream",
+    ]
+ 
+    resources = [
+    "*"
+    //  "arn:aws:s3:::codepipeline-us-west-2-627007557336"
+    ]
+  }
+ 
+  statement {
+    effect    = "Allow"
+    actions   = ["codestar-connections:UseConnection"]
+    resources = ["arn:aws:codestar-connections:us-west-2:962804699607:connection/5bbfbc52-4a3e-4124-a0ee-daf366f4ec2b"]
+  }
+ 
+  statement {
+    effect = "Allow"
+ 
+    actions = [
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild",
+    ]
+ 
+    resources = ["*"]
+  }
+}
+ 
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+ 
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+ 
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+
+resource "aws_codepipeline" "g5_codepipeline_capstone2_tf" {
+  name     = "g5-codepipeline-capstone2-tf"
+  role_arn = aws_iam_role.g5_codepipeline_role_tf.arn
+  
+  artifact_store {
+    location = "codepipeline-us-west-2-627007557336"
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      region           = "us-west-2"
+      version          = "1"
+      output_artifacts = ["g5-capstone2-source-artifact-tf"]
+
+      configuration = {
+        ConnectionArn    = "arn:aws:codestar-connections:us-west-2:962804699607:connection/5bbfbc52-4a3e-4124-a0ee-daf366f4ec2b"
+        FullRepositoryId = "guraylp3/g5-capstone2"
+        BranchName       = "main"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["g5-capstone2-source-artifact-tf"]
+      output_artifacts = ["g5-capstone2-build-artifact-tf"]
+      region           = "us-west-2"
+      version          = "1"
+
+      configuration = {
+        ProjectName = "g5-capstone2-codebuild-tf"
+      }
+    }
+  }
+/*
+  stage {
+    name = "Deploy"
+
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "CloudFormation"
+      input_artifacts = ["build_output"]
+      version         = "1"
+
+      configuration = {
+        ActionMode     = "REPLACE_ON_FAILURE"
+        Capabilities   = "CAPABILITY_AUTO_EXPAND,CAPABILITY_IAM"
+        OutputFileName = "CreateStackOutput.json"
+        StackName      = "MyStack"
+        TemplatePath   = "build_output::sam-templated.yaml"
+      }
+    }
+  }
+  */
+}
+
+/* // TODO: Do later if time
+resource "aws_codestarconnections_connection" "g5_capstone2_codestar_tf" {
+  name          = "g5-capstone2-codestar-tf"
+  provider_type = "GitHub"
+}
+*/
